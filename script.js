907bf992aecd21843deeeed77d2cf3472467e096
Mehmet Tiglioglu <mehmet.tiglioglu@turkishexporter.net>
2022-09-26 10:49:16 +0300

    Yorum satırları eklendi

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selectors
 let searchBar=document.querySelector(".search-bar input")
     randomButton=document.querySelector(".random-btn")
     countryName=document.querySelector(".city-name h1")
@@ -8,29 +9,32 @@ let searchBar=document.querySelector(".search-bar input")
     weatherCloudy=document.querySelector(".weather-cloudy");
 
 
-const clearWeather="https://p.kindpng.com/picc/s/178-1780507_sunny-weather-weather-line-icon-png-transparent-png.png"    
-
+// API URL
 const url="https://api.openweathermap.org/data/2.5/weather?q=";
 const urlKey="31e1cec39ebc5cec4f6ecd4dabf927e7";
 
+// Search inputtaki yazılan değeri alan ve enter'a basıldığında değeri başka bir fonksiyonda gönderen event.
 searchBar.addEventListener("keypress",(e)=>{
     if(e.keyCode===13){
         return getData(searchBar.value)
     }
 })
 
+// XMLHttpReqest nesnesi oluşturdum.
 let request=new XMLHttpRequest();
 
-
+// Veri gönderildiğinde çalışacak fonksiyon
 request.onreadystatechange=function(){
     if(request.readyState===4 && request.status===200)
     {
+        // Gelen veriler string olduğu için JSON formatına çevirdim.
         let jsonData=JSON.parse(request.responseText);
-        console.log(jsonData)
+
         countryName.innerHTML=`${jsonData.name}`
         countryTemp.innerHTML=`${Math.floor(jsonData.main.temp)} °`
         feelsTemp.innerHTML=`Hissedilen Sıcaklık : ${Math.floor(jsonData.main.feels_like)} °`
 
+        // Hava koşuluna göre resimlerin ekrana çıkmasını sağladım.
         if(jsonData.weather[0].main==="Rain"){
                 weatherImage.style="display:block"
                 weatherSunny.style="display:none";
@@ -56,16 +60,18 @@ request.onreadystatechange=function(){
     }
 }
 
+// Get Random Country buttonuna basıldıktan sonra random şehirin verilerinin gelmesini sağladım.
 randomButton.addEventListener("click",()=>{
    getData(randomCountry())
 })
 
+// Random şehir oluşturan fonksiyon
 function randomCountry(){
     let randomCity=["Wien","Paris","London","Porto","New York","Berlin","Milan","Napoli","Amsterdam","İstanbul","Prag","Roma","Kopenhag","Dublin","Hamburg"];
     return randomCity[Math.floor(Math.random() * randomCity.length)];
 }
 
-
+// Uzak sunucuya bağlanmak ve veri almak için gerekli kodlar.
 function getData(country){
     request.open("GET",`${url}+${country}+&appid=${urlKey}&units=metric&lang=tr`)
     request.send();
