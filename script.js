cfd49c061c66580b7fbd6215afb0cd29c60440af
Mehmet Tiglioglu <mehmet.tiglioglu@turkishexporter.net>
2022-09-26 09:51:58 +0300

    Random şehir seçmeyi sağlayan fonksiyonu yazıldı

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 let searchBar=document.querySelector(".search-bar input")
     randomButton=document.querySelector(".random-btn")
-    countryName=document.querySelector(".city-name")
-    countryTemp=document.querySelector(".city-temp");
+    countryName=document.querySelector(".city-name h1")
+    countryTemp=document.querySelector(".city-temp h1")
+    feelsTemp=document.querySelector(".feels-temp h1");
 
 const url="https://api.openweathermap.org/data/2.5/weather?q=";
 const urlKey="31e1cec39ebc5cec4f6ecd4dabf927e7";
 
 searchBar.addEventListener("keypress",(e)=>{
     if(e.keyCode===13){
-        return country(searchBar.value)
+        return getData(searchBar.value)
     }
 })
 
 let request=new XMLHttpRequest();
 
+
 request.onreadystatechange=function(){
     if(request.readyState===4 && request.status===200)
     {
         let jsonData=JSON.parse(request.responseText);
-        
+        console.log(jsonData)
         countryName.innerHTML=`${jsonData.name}`
-        
+        countryTemp.innerHTML=`${Math.floor(jsonData.main.temp)} °`
+        feelsTemp.innerHTML=`Hissedilen Sıcaklık : ${Math.floor(jsonData.main.feels_like)} °`
     }
 }
 
+randomButton.addEventListener("click",()=>{
+   getData(randomCountry())
+})
 
-function country(country){
+function randomCountry(){
+    let randomCity=["Wien","Paris","London","Porto","New York","Berlin","Milan","Napoli","Amsterdam","İstanbul","Prag","Roma","Kopenhag","Dublin","Hamburg"];
+    return randomCity[Math.floor(Math.random() * randomCity.length)];
+}
+
+
+function getData(country){
     request.open("GET",`${url}+${country}+&appid=${urlKey}&units=metric&lang=tr`)
-    request.send()
+    request.send();
 }
